29ea55308f9e80aa4af800f0ad1a6ae10474eeef
mehrdad <mnaser1992@gmail.com>
2025-05-29 00:46:35 +0330

    read all csv files

--- store-sales-time-series-forecasting/model.py
+++ store-sales-time-series-forecasting/model.py
@@ -1,5 +1,39 @@
 import pandas as pd
 
-df_train = pd.read_csv("dataset/train.csv")
+df_train = pd.read_csv(
+    "store-sales-time-series-forecasting/dataset/train.csv",
+    index_col='id',
+    parse_dates=['date'],
+)
 
-df_train.head()
+df_test = pd.read_csv(
+    "store-sales-time-series-forecasting/dataset/test.csv",
+    index_col='id',
+    parse_dates=['date'],
+)
+
+df_sample = pd.read_csv(
+    "store-sales-time-series-forecasting/dataset/sample_submission.csv",
+    index_col='id',
+)
+
+df_holidays = pd.read_csv(
+    "store-sales-time-series-forecasting/dataset/holidays_events.csv",
+    parse_dates=['date'],
+)
+
+df_oil = pd.read_csv(
+    "store-sales-time-series-forecasting/dataset/oil.csv",
+    parse_dates=['date'],
+)
+
+df_stores = pd.read_csv("store-sales-time-series-forecasting/dataset/stores.csv")
+
+df_transactions = pd.read_csv("store-sales-time-series-forecasting/dataset/transactions.csv")
+
+# get number of stores in train
+store_arr = df_train['store_nbr'].unique()
+store_nums = len(store_arr)
+
+for store in store_arr:
+  x_train = df_train.loc
