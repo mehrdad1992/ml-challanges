08b67d29ef04bcfbf60e1a9ab769fae1843e0d31
mehrdad <mnaser1992@gmail.com>
2025-05-29 23:11:36 +0330

    added time

--- store-sales-time-series-forecasting/model.py
+++ store-sales-time-series-forecasting/model.py
@@ -11,6 +11,8 @@ def main():
         index_col='id',
         parse_dates=['date'],
     )
+    df_train['Time'] = np.repeat(np.arange(1684),1782)
+
     # becaues of earthquake
     df_train = df_train[~((df_train['date'] >= "2016-05-16") & (df_train['date'] <= "2016-05-31"))]
 
@@ -58,38 +60,61 @@ def main():
     store_nums = len(store_arr)
 
     store_results = []
+    # for each store these will be done
     for store in store_arr:
+        # fetch x_train and y_train from df_train
         x_train_store = df_train[df_train['store_nbr'] == store]
         y_train_store = x_train_store['sales']
 
+        #fetch x_test_store from df_test
+        x_test_store = df_test[df_test['store_nbr'] == store]
+
+
+        # build x_train_store
         x_train_store['lag_1782'] = y_train_store.shift(1782)
         x_train_store['lag_1782'][0:1782] = x_train_store['lag_1782'][1782:1782*2] 
-        
+        x_train_store.drop('sales', axis=1, inplace=True)
+        #   x_train_store['oil'] = x_oil
+        x_train_store = x_train_store.merge(x_oil, on='date', how='left')
+        x_train_store['dcoilwtico'] = x_train_store['dcoilwtico'].fillna(method='bfill')
+        x_train_store = x_train_store.merge(df_transactions, on=['date', 'store_nbr'], how='left')
+        x_train_store['transactions'] = x_train_store['transactions'].fillna(0)
+        x_train_store = x_train_store.merge(df_stores, on='store_nbr', how='left')
+
+        # x_train_store = x_train_store.merge(df_holidays, on='date', how='left')
+
+        # build x_test_store
+        # x_train_store['lag_1782'] = y_train_store.shift(1782)
+        # x_train_store['lag_1782'][0:1782] = x_train_store['lag_1782'][1782:1782*2] 
+        x_test_store = x_test_store.merge(x_oil, on='date', how='left')
+        x_test_store['dcoilwtico'] = x_test_store['dcoilwtico'].fillna(method='bfill')
+        x_test_store = x_test_store.merge(df_transactions, on=['date', 'store_nbr'], how='left')
+        x_test_store['transactions'] = x_test_store['transactions'].fillna(0)
+        x_test_store = x_test_store.merge(df_stores, on='store_nbr', how='left')
+
+        le = LabelEncoder()
+        x_train_store['family'] = le.fit_transform(x_train_store['family'])
+        x_train_store['city'] = le.fit_transform(x_train_store['city'])
+        x_train_store['state'] = le.fit_transform(x_train_store['state'])
+        x_train_store['type'] = le.fit_transform(x_train_store['type'])
+
         ## drawing correlation plot of lag feature 
         # draw_plot(x_train_store['lag_1782'], y_train_store)
+
         model = LinearRegression()
         model.fit(x_train_store, y_train_store)
+
+        # get number of date in x_train
+        date_arr = x_train_store['date'].unique()
+        date_nums = len(date_arr)
+        for date in date_arr:
+            x_train_date = x_train_store[x_train_store['date'] == date]
+            x_test_date = x_test_store[x_test_store['date'] == date]
+
+
     
-        result = model.predict(x_test_store)
-        store_results.append(result)
-
-    x_train_store.drop('sales', axis=1, inplace=True)
-      #   x_train_store['oil'] = x_oil
-    x_train_store = x_train_store.merge(x_oil, on='date', how='left')
-    x_train_store['dcoilwtico'] = x_train_store['dcoilwtico'].fillna(method='bfill')
-    x_train_store = x_train_store.merge(df_transactions, on=['date', 'store_nbr'], how='left')
-    x_train_store['transactions'] = x_train_store['transactions'].fillna(0)
-    x_train_store = x_train_store.merge(df_stores, on='store_nbr', how='left')
-    # x_train_store = x_train_store.merge(df_holidays, on='date', how='left')
-    le = LabelEncoder()
-    x_train_store['family'] = le.fit_transform(x_train_store['family'])
-    x_train_store['city'] = le.fit_transform(x_train_store['city'])
-    x_train_store['state'] = le.fit_transform(x_train_store['state'])
-    x_train_store['type'] = le.fit_transform(x_train_store['type'])
-
-
-
-
+        # result = model.predict(x_test_store)
+        #store_results.append(result)
 
 def draw_plot(a, b):
     plt.plot(a, b, 'o')
