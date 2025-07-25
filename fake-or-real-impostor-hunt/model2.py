b95d4a9160792a16c8421a4149b24ecae05ffe7d
Hatef <hatefmdn@gmail.com>
2025-07-25 16:47:32 +0330

    Add latest changes

--- fake-or-real-impostor-hunt/model2.py
+++ fake-or-real-impostor-hunt/model2.py
@@ -19,9 +19,19 @@ def main():
             df_train.iat[i, 1] = temp
 
     df_train.columns = ['real', 'fake']
-    df_train['label'] = 0  # real is label 0
+    df_train['label'] = 1  # real is label 1
+
+    # mix real fake columns for train dataset
+    real_part = pd.DataFrame({'value': df_train['real'], 'label': df_train['label']})
+    fake_part = pd.DataFrame({'value': df_train['fake'], 'label': 0})
+    mixed_df_train = pd.concat([real_part, fake_part], ignore_index=True)    
+
+    # mix real fake columns for test dataset
+    mixed_df_test = pd.concat([df_test['file_1'], df_test['file_2']], ignore_index=True)    
+    mixed_df_test = pd.DataFrame(mixed_df_test, columns=['value'])
 
     # tokenized_test_1 = df_test['file_1'].map(lambda x: tokenizer(x, truncation=True, padding='max_length'))
+    
     # # tokenized_test_2 = df_test['file_2'].map(lambda x: tokenizer_test(x, truncation=True, padding='max_length'))
     # tokenized_ids_1 = tokenized_test_1.map(lambda x: x['input_ids'])
     # # tokenized_ids_2 = tokenized_test_2.map(lambda x: x['input_ids'])
@@ -30,8 +40,8 @@ def main():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     # Create huggingface dataset
-    dataset_train = Dataset.from_pandas(df_train[['real', 'label']].rename(columns={'real': 'c'}))
-    dataset_test = Dataset.from_pandas(df_test[['file_1']].rename(columns={'file_1': 'c'}))
+    dataset_train = Dataset.from_pandas(mixed_df_train[['value', 'label']].rename(columns={'value': 'c'}))
+    dataset_test = Dataset.from_pandas(mixed_df_test[['value']].rename(columns={'value': 'c'}))
 
     # Tokenize function
     def tokenize_function(examples):
@@ -58,7 +68,23 @@ def main():
     )
     trainer.train()
     predict = trainer.predict(tokenized_test)
-    pass
+    
+    logits = predict.predictions
+    predicted_real_indices = []
+
+    test_size = len(logits)//2
+
+    for i in range(test_size):
+        fake1, real1 = logits[i]
+        fake2, real2 = logits[test_size + i]
+        margin_1 = real1 - fake1
+        margin_2 = real2 - fake2
+        if margin_1 > margin_2:
+            predicted_real_indices.append(0)
+        else:
+            predicted_real_indices.append(1)
+
+    pd.Series(predicted_real_indices).to_csv("submission.csv", index=False, header=False)
 
 if __name__ == "__main__":
     main()
